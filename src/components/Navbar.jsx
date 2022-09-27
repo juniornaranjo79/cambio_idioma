a00b246b81cef9097af924984e51a5767a45b16f
Junior_naranjo <juniornaranjo79@gmail.com>
2022-09-26 19:55:12 -0500

    ejercicio clase react

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React, { useContext } from 'react'
+import "../styles.css"
+import LanguageContext from '../context'
+
+
+const Navbar = () => {
+    // DESCOMENTAR CUANDO EN APP.JSX TENGA UN PROVIDER
+    const { language, handleChangeLA } = useContext(LanguageContext)
+    const {text} = language // desestructurando language
+    return (
+        // ESPACIO DE TRABAJO, utilizar los valores de text
+        <div className="navbar">
+            <p>{ text.home }</p>
+            <p>{`${text.current} ${language.id}`}</p>
+            <button onClick={handleChangeLA}>{text.button}</button>
+        </div>
+    )
+}
+
+export default Navbar
